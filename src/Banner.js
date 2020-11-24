afbddc97d3898f748069f46f45848839a38f54a7
Roy <rdg01834@gmail.com>
2020-11-24 11:35:42 +0600

    complited banner, search, cards

--- src/Banner.js
+++ src/Banner.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Banner.css'
 import {Button} from "@material-ui/core"
+import Search from './Search';
 
 function Banner() {
+
+const [showSearch, setShowSearch] = useState(false);
+
     return (
         <div className = "banner">
+            <div className="banner__search">
+                {
+                    showSearch && <Search/>
+                }
+                <Button 
+                    className="banner__searchButton"
+                    variant = "outlined"
+                    onClick = { () =>
+                    setShowSearch(!showSearch)}>  
+                        {showSearch ? "Hide" : "Search Dates"}
+                </Button>
+            </div>
             <div className="banner__info">
                 <h1>Get out and stretch your imagination</h1>
                 <h5>
--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,44 @@
 import React from 'react';
 import Banner from './Banner';
 import './Home.css';
+import Card  from './Card'
+
 
 function Home() {
     return (
         <div className = "home">
             <Banner></Banner>
+
+            <div className="home__section">
+                <Card
+                    src = "https://wallpapercave.com/wp/wp6957431.jpg"
+                    title = "Blue Sweet Couple"
+                    description = "These rooms have been recently refurbished and are fully ensuite with toilet, shower and bath and toileteries."/>
+                <Card
+                    src = "https://wallpapercave.com/wp/wp6957481.jpg"
+                    title = "Beach House"
+                    description = "These rooms have been recently refurbished and are fully ensuite with toilet, shower and bath and toileteries."/>
+                <Card
+                    src = "https://wallpapercave.com/wp/wp6957478.jpg"
+                    title = "Garden Dual"
+                    description = "These rooms have been recently refurbished and are fully ensuite with toilet, shower and bath and toileteries."/>
+
+            </div>
+            <div className="home__section">
+                <Card
+                    src = "https://wallpapercave.com/wp/wp6957233.jpg"
+                    title = "Honeymoon Sweets"
+                    description = "These rooms have been recently refurbished and are fully ensuite with toilet, shower and bath and toileteries."/>
+                <Card
+                    src = "https://wallpapercave.com/wp/wp6957260.jpg"
+                    title = "Sky High"
+                    description = "These rooms have been recently refurbished and are fully ensuite with toilet, shower and bath and toileteries."/>
+                <Card
+                    src = "https://wallpapercave.com/wp/wp6957272.jpg"
+                    title = "Queen Size"
+                    description = "These rooms have been recently refurbished and are fully ensuite with toilet, shower and bath and toileteries."/>
+
+            </div>
         </div>
     )
 }
